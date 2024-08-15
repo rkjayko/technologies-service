5da987113105a3831e6ceb95388942bf66e27c52
Jaime Mejia <rkjayko2@gmail.com>
2024-08-15 09:00:33 -0500

    Scaffolding technologies-service

---
+++ src/test/java/com/example/technologies_service/TechnologiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technologies_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologiesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
